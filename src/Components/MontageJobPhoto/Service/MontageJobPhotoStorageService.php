dd500b4a51f3d0d869fd5da4ac4771afa41ccc32
hrayr <developer.hovakimyan@gmail.com>
2024-10-18 20:45:07 +0400

    Create MontagePhotoStorageService class
    MontagePhotoStorageService abstract class
    is created for avoiding code duplication.

--- src/Components/MontageJobPhoto/Service/MontageJobPhotoStorageService.php
+++ src/Components/MontageJobPhoto/Service/MontageJobPhotoStorageService.php
@@ -2,47 +2,14 @@
 
 namespace Riconas\RiconasApi\Components\MontageJobPhoto\Service;
 
-use Riconas\RiconasApi\Utility\StringUtility;
+use Riconas\RiconasApi\Components\MontageJob\Service\MontagePhotoStorageService;
 
-class MontageJobPhotoStorageService
+class MontageJobPhotoStorageService extends MontagePhotoStorageService
 {
-    private const BASE_ABSOLUTE_PATH = 'data/uploads/montage_jobs/photos';
     private const BASE_RELATIVE_PATH = 'montage_jobs/photos';
 
-    public function getPhotoUrl(string $photoFileName): string
+    public function __construct()
     {
-        $photoUrlParts = [
-            $_ENV['UPLOADS_DOMAIN'],
-            self::BASE_RELATIVE_PATH,
-            $photoFileName,
-        ];
-
-        return implode('/', $photoUrlParts);
-    }
-
-    public function getPathForUploadedPhotoFile(string $photoFileName): string
-    {
-        $uploadedFileExt = pathinfo($photoFileName, PATHINFO_EXTENSION);
-
-        $randomString = StringUtility::generateRandomString(20);
-        $targetFileName = "{$randomString}_photo.{$uploadedFileExt}";
-
-        return $this->getPhotoAbsolutePath($targetFileName);
-    }
-
-    public function deletePhotoFile(string $photoFileName): bool
-    {
-        $fileAbsolutePath = $this->getPhotoAbsolutePath($photoFileName);
-
-        if (unlink($fileAbsolutePath)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private function getPhotoAbsolutePath(string $photoFileName): string
-    {
-        return $_ENV['ROOT_FILESYSTEM_PATH'] . '/' . self::BASE_ABSOLUTE_PATH . '/' . $photoFileName;
+        parent::__construct(self::BASE_RELATIVE_PATH);
     }
 }
--- src/Components/MontageOntPhoto/Service/MontageOntPhotoStorageService.php
+++ src/Components/MontageOntPhoto/Service/MontageOntPhotoStorageService.php
@@ -2,47 +2,14 @@
 
 namespace Riconas\RiconasApi\Components\MontageOntPhoto\Service;
 
-use Riconas\RiconasApi\Utility\StringUtility;
+use Riconas\RiconasApi\Components\MontageJob\Service\MontagePhotoStorageService;
 
-class MontageOntPhotoStorageService
+class MontageOntPhotoStorageService extends MontagePhotoStorageService
 {
     private const BASE_RELATIVE_PATH = 'montage_jobs/ont_photos';
-    private const BASE_ABSOLUTE_PATH = "data/uploads/" . self::BASE_RELATIVE_PATH;
 
-    public function getPhotoUrl(string $photoFileName): string
+    public function __construct()
     {
-        $photoUrlParts = [
-            $_ENV['UPLOADS_DOMAIN'],
-            self::BASE_RELATIVE_PATH,
-            $photoFileName,
-        ];
-
-        return implode('/', $photoUrlParts);
-    }
-
-    public function getPathForUploadedPhotoFile(string $photoFileName): string
-    {
-        $uploadedFileExt = pathinfo($photoFileName, PATHINFO_EXTENSION);
-
-        $randomString = StringUtility::generateRandomString(20);
-        $targetFileName = "{$randomString}_photo.{$uploadedFileExt}";
-
-        return $this->getPhotoAbsolutePath($targetFileName);
-    }
-
-    public function deletePhotoFile(string $photoFileName): bool
-    {
-        $fileAbsolutePath = $this->getPhotoAbsolutePath($photoFileName);
-
-        if (unlink($fileAbsolutePath)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private function getPhotoAbsolutePath(string $photoFileName): string
-    {
-        return $_ENV['ROOT_FILESYSTEM_PATH'] . '/' . self::BASE_ABSOLUTE_PATH . '/' . $photoFileName;
+        parent::__construct(self::BASE_RELATIVE_PATH);
     }
 }
